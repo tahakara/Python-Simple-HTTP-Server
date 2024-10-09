e3e14eac6019a589d2fe7fc6e9d0315b66c43584
Taha Kara <74018704+tahakara@users.noreply.github.com>
2024-10-09 19:15:38 +0300

    .

---
+++ Alpha.py
@@ -0,0 +1,17 @@
+import http.server
+import socketserver
+
+# Port numarası
+PORT = int(input("Port number: "))
+
+class SimpleHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+
+        self.wfile.write(b"Hello, this is a simple HTTP server!")
+
+with socketserver.TCPServer(("", PORT), SimpleHTTPRequestHandler) as httpd:
+    print(f"Serving on port {PORT}")
+    httpd.serve_forever()
